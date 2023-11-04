3585d4a1bbc2273a53229e29b61bb87beab18054
Programing Hero <sajalb1899@gmail.com>
2023-11-04 23:07:49 +0600

    create nav bar & added

--- src/component/NavBar/NavBar.jsx
+++ src/component/NavBar/NavBar.jsx
@@ -1,8 +1,161 @@
+import { useEffect } from "react";
+import { Link, NavLink } from "react-router-dom";
+// import { AuthContext } from "../AuthProvider/AuthProvider";
+// import { BsBrightnessHigh } from 'react-icons/bs';
+// import { MdDarkMode } from "react-icons/md";
 
 const NavBar = () => {
+
+    // const { userSignOut, user } = useContext(AuthContext)
+    // const [mode, setMode] = useState("light");
+
+    // const handleToggle = () => {
+    //     const html = document.documentElement
+    //     if (mode == "light") {
+    //         html.classList.remove("light")
+    //         html.classList.add("dark")
+    //         setMode("dark")
+    //         localStorage.setItem("mode", "dark")
+    //     }
+    //     else {
+    //         html.classList.remove("dark")
+    //         html.classList.add("light")
+    //         setMode("light")
+    //         localStorage.setItem("mode", "light")
+    //     }
+    // }
+    useEffect(() => {
+        const currentValue = localStorage.getItem("mode") || "light";
+        // setMode(currentValue)
+        const html = document.documentElement
+        html.classList.add(currentValue)
+    }, [])
+
     return (
-        <div>
-            <h3>This is a navbar</h3>
+        <div className="dark:bg-black">
+            <div className="navbar bg-base-100 dark:bg-black lg:w-10/12 md:w-10/12 m-auto ">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                            <li><NavLink className="md:text-xl " to={'/'} style={({ isActive }) => {
+                                return {
+                                    fontWeight: isActive ? "bold" : "",
+                                    color: isActive ? "red" : "",
+                                    backgroundColor: isActive ? 'white' : '',
+                                    textDecoration: isActive ? "underline" : '',
+                                };
+                            }}>Home</NavLink></li>
+                            <li><NavLink className="md:text-xl " to={'/addblog'} style={({ isActive }) => {
+                                return {
+                                    fontWeight: isActive ? "bold" : "",
+                                    color: isActive ? "red" : "",
+                                    backgroundColor: isActive ? 'white' : '',
+                                    textDecoration: isActive ? "underline" : '',
+                                };
+                            }}>Add Blog</NavLink></li>
+                            <li><NavLink className="md:text-xl " to={'/allblog'} style={({ isActive }) => {
+                                return {
+                                    fontWeight: isActive ? "bold" : "",
+                                    color: isActive ? "red" : "",
+                                    backgroundColor: isActive ? 'white' : '',
+                                    textDecoration: isActive ? "underline" : '',
+                                };
+                            }}>All blog</NavLink></li>
+                            <li><NavLink className="md:text-xl " to={'/featureblog'} style={({ isActive }) => {
+                                return {
+                                    fontWeight: isActive ? "bold" : "",
+                                    color: isActive ? "red" : "",
+                                    backgroundColor: isActive ? 'white' : '',
+                                    textDecoration: isActive ? "underline" : '',
+                                };
+                            }}>Feature Blog</NavLink></li>
+                            <li><NavLink className="md:text-xl " to={'/wishlist'} style={({ isActive }) => {
+                                return {
+                                    fontWeight: isActive ? "bold" : "",
+                                    color: isActive ? "red" : "",
+                                    backgroundColor: isActive ? 'white' : '',
+                                    textDecoration: isActive ? "underline" : '',
+                                };
+                            }}> Wishlist</NavLink></li>
+                        </ul>
+                    </div>
+                    <div className="flex items-center">
+                        <img className="w-16" src="" alt="" />
+                        <Link to={'/'}><p className="text-3xl font-bold flex flex-col">Hello Blog</p></Link>
+                    </div>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        <li><NavLink className="md:text-xl " to={'/'} style={({ isActive }) => {
+                            return {
+                                fontWeight: isActive ? "bold" : "",
+                                color: isActive ? "red" : "",
+                                backgroundColor: isActive ? 'white' : '',
+                                textDecoration: isActive ? "underline" : '',
+                            };
+                        }}>Home</NavLink></li>
+                        <li><NavLink className="md:text-xl " to={'/addblog'} style={({ isActive }) => {
+                            return {
+                                fontWeight: isActive ? "bold" : "",
+                                color: isActive ? "red" : "",
+                                backgroundColor: isActive ? 'white' : '',
+                                textDecoration: isActive ? "underline" : '',
+                            };
+                        }}>Add Blog</NavLink></li>
+                        <li><NavLink className="md:text-xl " to={'/allbolg'} style={({ isActive }) => {
+                            return {
+                                fontWeight: isActive ? "bold" : "",
+                                color: isActive ? "red" : "",
+                                backgroundColor: isActive ? 'white' : '',
+                                textDecoration: isActive ? "underline" : '',
+                            };
+                        }}>All Blog</NavLink></li>
+                        <li><NavLink className="md:text-xl " to={'/featureblog'} style={({ isActive }) => {
+                            return {
+                                fontWeight: isActive ? "bold" : "",
+                                color: isActive ? "red" : "",
+                                backgroundColor: isActive ? 'white' : '',
+                                textDecoration: isActive ? "underline" : '',
+                            };
+                        }}>Feature Blog</NavLink></li>
+                        <li><NavLink className="md:text-xl " to={'/wishlist'} style={({ isActive }) => {
+                            return {
+                                fontWeight: isActive ? "bold" : "",
+                                color: isActive ? "red" : "",
+                                backgroundColor: isActive ? 'white' : '',
+                                textDecoration: isActive ? "underline" : '',
+                            };
+                        }}> Wishlist</NavLink></li>
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    {/* <div onClick={handleToggle} className="text-3xl pr-2 flex">
+                        {
+                            mode == "light" ? <MdDarkMode></MdDarkMode> : <BsBrightnessHigh className="text-white"></BsBrightnessHigh>
+                        }
+                    </div> */}
+
+                    {/* {
+                        user ? <>
+                            <div className='flex items-center gap-3 pr-4'>
+                                <p className='text-xs'>{user.displayName}</p>
+                                <div className="dropdown dropdown-end">
+                                    <label tabIndex={0} className=""><img className='w-10 h-10 m-auto rounded-full' src={user.photoURL === null ? "https://i.ibb.co/VB17ZCv/user.png" : user?.photoURL} alt="" /></label>
+                                    <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+                                        <li><a onClick={userSignOut}>Sign Out</a></li>
+                                    </ul>
+                                </div>
+
+                            </div>
+                            <Link ><button onClick={userSignOut} className="text-white bg-stone-700 rounded-lg px-5 md:block hidden py-2 text-lg font-bold">LogOut</button> </Link>
+                        </>
+                            : <Link to={'/logIn'} className="text-white bg-stone-700 rounded-lg px-5  py-2 text-lg font-bold">LogIn</Link>
+                    } */}
+                </div>
+            </div>
         </div>
     );
 };
