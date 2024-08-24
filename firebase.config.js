92ec31edf70786f7232c1a9367bf82dc1c9b445e
sajalbiswas1 <sajalb1899@gmail.com>
2024-08-24 22:33:56 +0600

    update banner

--- firebase.config.js
+++ firebase.config.js
@@ -7,12 +7,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey:import.meta.env.VITE_SOME_KEY_apiKey,
-  authDomain:import.meta.env.VITE_SOME_KEY_authDomain,
-  projectId:import.meta.env.VITE_SOME_KEY_projectId,
-  storageBucket:import.meta.env.VITE_SOME_KEY_storageBucket,
-  messagingSenderId:import.meta.env.VITE_SOME_KEY_messagingSenderId,
-  appId:import.meta.env.VITE_SOME_KEY_appId
+  apiKey: import.meta.env.VITE_SOME_KEY_apiKey,
+  authDomain: import.meta.env.VITE_SOME_KEY_authDomain,
+  projectId: import.meta.env.VITE_SOME_KEY_projectId,
+  storageBucket: import.meta.env.VITE_SOME_KEY_storageBucket,
+  messagingSenderId: import.meta.env.VITE_SOME_KEY_messagingSenderId,
+  appId: import.meta.env.VITE_SOME_KEY_appId,
 };
 
 // Initialize Firebase
--- src/component/Banner/Banner.jsx
+++ src/component/Banner/Banner.jsx
@@ -1,19 +1,29 @@
-
 const Banner = () => {
-    return (
-        <div className="hero min-h-screen" style={{backgroundImage: 'url(https://i.ibb.co/jGVKrTd/43d3c663-e800-45d4-89c5-5a3e567cc12c.jpg)'}}>
-  <div className="hero-overlay bg-opacity-60"></div>
-  <div className="hero-content text-center text-white">
-    <div className="">
-      <h1 className="mb-5 text-5xl font-bold">Share your Mind With BloggerEx</h1>
-      <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <button className="btn btn-primary">Get Started</button>
+  return (
+    <div
+    // className="hero min-h-screen"
+    // style={{
+    //   backgroundImage:
+    //     "url(https://i.ibb.co/jGVKrTd/43d3c663-e800-45d4-89c5-5a3e567cc12c.jpg)",
+    // }}
+    >
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content text-center text-white">
+        <div className="">
+          <h1 className="mb-5 text-5xl font-bold">
+            Share your Mind With BloggerEx
+          </h1>
+          <p className="mb-5">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <button className="btn btn-primary">Get Started</button>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-
-
-    );
+  );
 };
 
-export default Banner; 50
+export default Banner;
+50;
